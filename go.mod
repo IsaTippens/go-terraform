fc70d919b5511ec66aa30587ffa516b5367b9747
IsaTippens <isatippens2@gmail.com>
2025-11-05 15:51:29 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/IsaTippens/go-terraform
+
+go 1.23.3
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Name string `yaml:"name"`
+}
+
+func loadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func main() {
+	// load in config file
+	// format:
+	// - name: string
+	//
+	// start http server
+	// -endpoint /health returns 200 OK
+	// -endpoint / returns "Hello {config.name}"
+
+	config, err := loadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello " + config.Name))
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
